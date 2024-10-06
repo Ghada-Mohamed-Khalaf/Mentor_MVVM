74369735ee97044652a37238a168432c52d2fee9
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-06 23:47:38 +0300

    wrap column with padding

--- lib/features/auth/presentation/views/splash_view.dart
+++ lib/features/auth/presentation/views/splash_view.dart
@@ -13,27 +13,30 @@ class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset("assets/images/img.png"),
-          Text("The best chat app of this century"),
-          SizedBox(height: 50,),
-          CustomButton(onTap: () {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-    return LoginView();
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/img.png"),
+            Text("The best chat app of this century"),
+            SizedBox(height: 50,),
+            CustomButton(onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return LoginView();
 
 
-    }));
-    },
-            text: "continue"),
+            }));
+            },
+              text: "continue"),
 
 
 
 
-        ],
+          ],
 
+        ),
       ),
 
     );
