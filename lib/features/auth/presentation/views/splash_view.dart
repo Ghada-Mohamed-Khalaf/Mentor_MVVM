f9cb49f29299accea41af430312179bc1fa9329a
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-06 22:16:12 +0300

    first commit

---
+++ lib/features/auth/presentation/views/splash_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/img.png"),
+         // Text("The best chat app of this century")
+
+        ],
+
+      ),
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_mentor/features/auth/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashView(),
+    );
+  }
+}
+
+
