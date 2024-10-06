a591c4d8c5325b4a15d8e39e1dfb883ec7dcce92
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-06 23:45:59 +0300

    add custom button

--- lib/features/auth/presentation/views/splash_view.dart
+++ lib/features/auth/presentation/views/splash_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_mentor/features/auth/presentation/views/Login_view.dart';
+import 'package:mvvm_mentor/features/auth/presentation/views/widgets/custom_button.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -17,6 +19,18 @@ class _SplashViewState extends State<SplashView> {
         children: [
           Image.asset("assets/images/img.png"),
           Text("The best chat app of this century"),
+          SizedBox(height: 50,),
+          CustomButton(onTap: () {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+    return LoginView();
+
+
+    }));
+    },
+            text: "continue"),
+
+
+
 
         ],
 
