7b43d7df28abf0557ab33e272a8bab19a2a1decb
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-08 23:07:11 +0300

    create whatsUp view

---
+++ lib/features/auth/presentation/views/whats_up.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WhatsUp extends StatelessWidget {
+  const WhatsUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
