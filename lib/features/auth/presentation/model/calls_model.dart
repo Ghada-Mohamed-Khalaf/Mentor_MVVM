291fb61281e0b1d7b16d0fdd935f38e1170d32d0
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-15 22:35:46 +0300

    create calls model

--- lib/features/auth/presentation/model/calls_model.dart
+++ lib/features/auth/presentation/model/calls_model.dart
@@ -1,14 +1,14 @@
-// import 'package:flutter/cupertino.dart';
-//
-// class CallsModel {
-//   String image;
-//
-//   String date;
-//   String name;
-//
-//   CallsModel(
-//       {required this.date,
-//       required this.image,
-//
-//       required this.name});
-// }
+import 'package:flutter/cupertino.dart';
+
+class CallsModel {
+  String image;
+
+  String date;
+  String name;
+
+  CallsModel(
+      {required this.date,
+      required this.image,
+
+      required this.name});
+}
